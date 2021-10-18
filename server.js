16adf0128e47083f8275d25b34b268442671d5ce
Drew Schmidtke <amschmidtke@gmail.com>
2021-10-18 16:46:15 -0400

    intial page loading with working button

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
-
+const path = require('path');
 const app = express()
+const PORT = process.env.PORT || 3001;
 
-app.listen(3001, () => {
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+const notes = require('./db/db.json');
+
+
+app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.listen(PORT, function () {
     console.log(`API server now on port 3001`);
 });
